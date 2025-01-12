b5f0380700d5a58aab52fae2523555b59798da0a
Emmett <emmettfunston@ucsb.edu>
2025-01-11 17:52:43 -0800

    Implement location tracking functionality - Add LocationManager class with background location updates - Configure location permissions and tracking - Enable continuous location monitoring - Add authorization status handling

--- HeatMap-UCSB-Hackathon/MapViewWrapper.swift
+++ HeatMap-UCSB-Hackathon/MapViewWrapper.swift
@@ -1,24 +1,85 @@
 import SwiftUI
 import MapboxMaps
+import CoreLocation
 
 struct MapViewWrapper: UIViewRepresentable {
+    @StateObject private var locationManager = LocationManager()
+    
     func makeUIView(context: Context) -> MapView {
         let options = MapInitOptions(
             resourceOptions: ResourceOptions(accessToken: "pk.eyJ1IjoiZW1tZXR0ZnVuc3RvbiIsImEiOiJjbTVzbjJoZDEwb2o4MmxwbGxyZndvejd3In0.6LtI5A_OiRBniV0QzMauBA"),
+            cameraOptions: CameraOptions(
+                center: CLLocationCoordinate2D(latitude: 34.4140, longitude: -119.8489),
+                zoom: 14.0
+            ),
             styleURI: .streets
         )
         
         let mapView = MapView(frame: .zero, mapInitOptions: options)
         
-        // Center the map on UCSB campus
-        let ucsbCoordinate = CLLocationCoordinate2D(latitude: 34.4140, longitude: -119.8489)
-        let cameraOptions = CameraOptions(center: ucsbCoordinate, zoom: 15.0)
-        mapView.camera.fly(to: cameraOptions, duration: 1.0)
+        // Enable location tracking
+        mapView.location.options.puckType = .puck2D()
+        locationManager.startUpdating()
         
         return mapView
     }
     
     func updateUIView(_ uiView: MapView, context: Context) {
-        // Updates can be handled here
+    }
+}
+
+// Location Manager class
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private let locationManager = CLLocationManager()
+    @Published var location: CLLocation?
+    
+    override init() {
+        super.init()
+        setupLocationManager()
+    }
+    
+    private func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = 10 // Update every 10 meters
+        
+        // Request authorization first
+        locationManager.requestAlwaysAuthorization()
+        
+        // Enable background updates
+        locationManager.allowsBackgroundLocationUpdates = true
+        locationManager.showsBackgroundLocationIndicator = true
+        locationManager.pausesLocationUpdatesAutomatically = false
+    }
+    
+    func startUpdating() {
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        self.location = location
+        print("Location updated: \(location.coordinate.latitude), \(location.coordinate.longitude)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location error: \(error.localizedDescription)")
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .authorizedAlways:
+            print("Always authorization granted")
+            startUpdating()
+        case .authorizedWhenInUse:
+            print("When in use authorization granted - requesting always authorization")
+            locationManager.requestAlwaysAuthorization()
+        case .denied, .restricted:
+            print("Location access denied or restricted")
+        case .notDetermined:
+            print("Location authorization not determined")
+        @unknown default:
+            print("Unknown authorization status")
+        }
     }
 }
