1734f92465085a62fba52a0e713b58eaa34f4b25
Luke Herbelin <herbelinluke@gmail.com>
2025-01-11 16:37:02 -0800

    test firebase initialization

--- HeatMap-UCSB-Hackathon/HeatMap_UCSB_Hackathon__25App.swift
+++ HeatMap-UCSB-Hackathon/HeatMap_UCSB_Hackathon__25App.swift
@@ -6,9 +6,22 @@
 //
 
 import SwiftUI
+import FireBaseCore
+
+// create an app delegate
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct HeatMap_UCSB_Hackathon__25App: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
