5b17cce170a3a7480e2ab4b300f42db31ab99ed7
Luke Herbelin <herbelinluke@gmail.com>
2025-01-11 16:57:48 -0800

    test adding documents to database

--- HeatMap-UCSB-Hackathon/ViewController.swift
+++ HeatMap-UCSB-Hackathon/ViewController.swift
@@ -12,4 +12,38 @@ class ViewController: UIViewController {
         mapView = MapView(frame: view.bounds, mapInitOptions: options)
         view.addSubview(mapView)
     }
+
+    // Add a new document with a generated ID
+    do {
+      let ref = try await db.collection("users").addDocument(data: [
+        "first": "Ada",
+        "last": "Lovelace",
+        "born": 1815
+      ])
+      print("Document added with ID: \(ref.documentID)")
+    } catch {
+      print("Error adding document: \(error)")
+    }
+
+    // Add a second document with a generated ID.
+    do {
+    let ref = try await db.collection("users").addDocument(data: [
+    "first": "Alan",
+    "middle": "Mathison",
+    "last": "Turing",
+    "born": 1912
+    ])
+    print("Document added with ID: \(ref.documentID)")
+    } catch {
+    print("Error adding document: \(error)")
+    }
+
+    do {
+      let snapshot = try await db.collection("users").getDocuments()
+      for document in snapshot.documents {
+        print("\(document.documentID) => \(document.data())")
+      }
+    } catch {
+      print("Error getting documents: \(error)")
+    }
 } 
